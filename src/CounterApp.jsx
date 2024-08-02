ef21c0851ff4d634e39e7057a7e36180451f67bd
Alejandro Raya <alejandroraya09@hotmail.com>
2024-08-01 23:20:46 -0600

    Añadir y Resetear con Hooks -UseState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-
-
-
 export const CounterApp = ({value}) => {
     const [ counter, setCounter ] = useState( value );
 
@@ -12,18 +9,19 @@ export const CounterApp = ({value}) => {
     const handleAdd = () => {
         // setCounter(counter + 1 )
         setCounter((C) => C + 1)
-     }
+    }
 
-     
+    const handleSubstract = () => setCounter (counter - 1)
+    const handleReset = () => setCounter (value)
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
 
 
-            <button onClick={handleAdd}>
-                +1
-            </button>
+            <button onClick={handleAdd}> +1</button>
+            <button onClick={handleReset}> Reset </button>
+            <button onClick={handleSubstract}> -1 </button>
         </>
       );
 }
