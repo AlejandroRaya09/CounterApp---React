c6b53280386184b8dc32b7043a832f7aa7f7949a
Alejandro Raya <alejandroraya09@hotmail.com>
2024-08-01 23:13:04 -0600

    UseState - Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { FirstApp } from "./FirstApp";
 import "./style.css";
+import {CounterApp} from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <FirstApp title="Hola Soy un titulo" subtitulo="SSUBTITULO"/> */}
-    <FirstApp title="Hola Soy un titulo" subtitulo=" "/>
+    {/* <FirstApp title="Hola Soy un titulo" subtitulo=" "/> */}
+    <CounterApp value={123}/>
   </React.StrictMode>
 );
